00f1318ff857fef3f19f1715cd56eb8917a13275
Leonardo Luiz <leonardokl@hotmail.com>
2016-08-12 23:48:21 -0300

    Modify styles making the extension more similar to spotify webapp.
    Show track artist and name.
    Indicate the song state, playing or paused.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
 var MAIN_URL = 'https://play.spotify.com/*';
 var ALBUMS_URL = 'https://play.spotify.com/collection/albums';
+var APP_PLAYER = 'document.getElementById("app-player").contentDocument';
 //TODO: SEARCH_URL = https://play.spotify.com/search/nirvana
 
 document.addEventListener('DOMContentLoaded', function() {
-	
+
 	updateTrackInfo();
 
 	// Add events to buttons;
   	document.getElementById('open').addEventListener('click', function() {
-  		
+
   		getCurrentTab(function(tabs){
   			// Create a spotify tab if one doesn't yet exists.
 		    if (tabs.length === 0) {
 		      	chrome.tabs.create({url: ALBUMS_URL});
 		    }
 		    else{
-		    	chrome.tabs.update(tabs[0].id, {highlighted: true});	
-		    }	
+		    	chrome.tabs.update(tabs[0].id, {highlighted: true});
+		    }
   		});
   	}, false);
 
@@ -43,7 +44,7 @@ function execute(action){
 	      	chrome.tabs.create({url: ALBUMS_URL});
 	    }
 	    else {
-	    	
+
 		    // Apply action on all spotify tabs.
 		    // get iframe 'app-player' and then find the right action button;
 		    for (var tab of tabs) {
@@ -56,18 +57,25 @@ function execute(action){
 		    setTimeout(function(){
 		    	updateTrackInfo();
 		    },2000);
-		    
+
 		    // Unload background page as soon as we're done.
-		    //window.close();	
+		    //window.close();
 	    }
 	});
+}
+
+function getAppPlayerItem() {
+
 }
 
 /**
  * Show current trackname if there's one;
  */
 function updateTrackInfo(){
-	
+	// var appPlayer = document.getElementById('app-player').contentDocument;
+	// var trackName = appPlayer.getElementById('track-name').getElementsByTagName("a")[0].innerHTML;
+	// var trackArtist = appPlayer.getElementById('track-artist').getElementsByTagName("a")[0].innerHTML;
+
 	getCurrentTab(function(tabs){
 
 		if (tabs.length === 0) {
@@ -75,23 +83,57 @@ function updateTrackInfo(){
 	      	document.querySelector('#album-art').innerHTML = "";
 	    }
 	    else {
-	    	//update trackname;
-			document.querySelector('#current-track-name').innerHTML = tabs[0].title;
-
-			//var code = "document.getElementById('app-player').contentDocument.getElementById('cover-art');";
-		    //var code = "document.getElementById('app-player').contentDocument.getElementById('cover-art').querySelector('.sp-image-img').outerHTML;";
-		    //var code = "document.getElementById('app-player').contentDocument.getElementById('cover-art').querySelector('.sp-image-img').style.backgroundImage;";
-
 		    //show album cover;
 		    for (var tab of tabs) {
 		      	chrome.tabs.executeScript(tab.id, {
-		      		code: "document.getElementById('app-player').contentDocument.getElementById('cover-art').querySelector('.sp-image-img').style.backgroundImage;"
-		      	}, 
+		      		code: APP_PLAYER + '.getElementById("cover-art").querySelector(".sp-image-img").style.backgroundImage;'
+		      	},
 		      	function(resp){
 			      	//resp[0] contains the result of code executed on target tab.id, where will be returned the background-image attribute;
 			      	var src = resp[0].replace('url(','').replace(')','');
 			      	document.querySelector('#album-art').innerHTML = '<img src='+src+' style="width:200px; height:200px;">';
-			    });
+			    	});
+
+						chrome.tabs.executeScript(tab.id, {
+		      		code: APP_PLAYER + '.getElementById("track-name").getElementsByTagName("a")[0].innerHTML;'
+		      	},
+		      	function(resp){
+							//alert(String(resp))
+							if (resp) {//alert('""' + String(resp) + '"')
+								document.querySelector('#current-track-name').innerHTML = resp;
+							}
+			    	});
+
+						chrome.tabs.executeScript(tab.id, {
+		      		code: APP_PLAYER + '.getElementById("track-artist").getElementsByTagName("a")[0].innerHTML;'
+		      	},
+		      	function(resp){
+							if (resp) {
+								document.querySelector('#current-track-artist').innerHTML = resp;
+							}
+			    	});
+
+						chrome.tabs.executeScript(tab.id, {
+		      		code: 'document.getElementById("app-player").contentDocument.getElementById("play-pause").className;'
+		      	},
+		      	function(resp){
+							if (resp) {
+								var playClassNames = String(resp).split(" ");
+								var newPlayContent = '<span class="glyphicon glyphicon glyphicon-play"></span>';
+
+								playClassNames.forEach(function (className) {
+									switch (className) {
+										case "playing":
+											newPlayContent = '<span class="glyphicon glyphicon glyphicon-pause"></span>';
+											break;
+										case "disabled":
+											newPlayContent = '<span class="glyphicon glyphicon glyphicon-remove"></span>';
+									}
+								});
+
+								document.querySelector('#play-pause').innerHTML = newPlayContent;
+							}
+			    	});
 		    }
 		}
 	});
@@ -101,6 +143,7 @@ function updateTrackInfo(){
  * Look for tabs with url like MAIN_URL;
  */
 function getCurrentTab(successFunction){
-	
+
 	chrome.tabs.query({url: MAIN_URL}, successFunction);
 }
+// document.getElementById("app-player").contentDocument.getElementById("play-pause").className
