61be9233eb3b1682a02d374f32f16fd55bd0ff6f
Chuma Umenze <donbona8@gmail.com>
2018-11-21 19:09:10 +0100

    Fix Control buttons

--- js/background.js
+++ js/background.js
@@ -1,13 +1,13 @@
 // Set up context menu at install time.
-chrome.runtime.onInstalled.addListener(function() {
-  	chrome.contextMenus.create({
-		"title": "Play/Pause Spotify",
-	  	"contexts":["all"],
-	  	"id": "context" + "all"
-	});
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.contextMenus.create({
+        "title": "Play/Pause Spotify",
+        "contexts": ["all"],
+        "id": "context" + "all"
+    });
 });
 
 // Add click event
-chrome.contextMenus.onClicked.addListener(function(/*info, tab*/){
-	handlePlayOrPauseClick(); // calls main.js method;
+chrome.contextMenus.onClicked.addListener(function (/*info, tab*/) {
+    handlePlayOrPauseClick(); // calls main.js method;
 });
--- js/main.js
+++ js/main.js
@@ -1,33 +1,33 @@
 /* globals XMLHttpRequest, chrome */
 
-var MAIN_URL = 'https://open.spotify.com/*'
-var ALBUMS_URL = 'https://open.spotify.com/collection/albums'
-var APP_PLAYER = 'document.getElementById("app-player").contentDocument'
-var VAGALUME_API = '4b426abf3e83723a3f0ba2dedc63e6e2'
+var MAIN_URL = 'https://open.spotify.com/*';
+var ALBUMS_URL = 'https://open.spotify.com/collection/albums';
+var APP_PLAYER = 'document.getElementById("app-player").contentDocument';
+var VAGALUME_API = '4b426abf3e83723a3f0ba2dedc63e6e2';
 
 // utils
-function findEl (path) {
+function findEl(path) {
   return document.querySelector(path)
 }
 
-function onClick (el, callback) {
+function onClick(el, callback) {
   return el.addEventListener('click', callback)
 }
 
 var Chrome = {
   executeScript: function (options, callback) {
-    var tab = options.tab
-    var code = options.code
+    var tab = options.tab;
+    var code = options.code;
 
     chrome.tabs.executeScript(tab.id, {
       code: code
     }, callback)
   }
-}
+};
 
 var State = {
   tabs: []
-}
+};
 
 var Spotify = {
   createTab: function () {
@@ -45,9 +45,9 @@ var Spotify = {
   getAlbumArt: function (tab, callback) {
     Chrome.executeScript({
       tab: tab,
-      code: 'document.querySelector(".nowPlayingBar-container .cover-art-image").style.backgroundImage'
+      code: 'document.querySelector(".now-playing-bar-container .cover-art-image.cover-art-image-loaded").style.backgroundImage'
     }, function (res) {
-      var albumArt = res[0].replace('url(', '').replace(')', '')
+      var albumArt = res[0].replace('url(', '').replace(')', '');
 
       callback(albumArt)
     })
@@ -70,18 +70,18 @@ var Spotify = {
   getPlayOrPauseStatus: function (tab, callback) {
     Chrome.executeScript({
       tab: tab,
-      code: 'document.querySelector(\'.nowPlayingBar-container .player-controls button[class^="control-button spoticon-pause"]\')'
+      code: 'document.querySelector(\'.now-playing-bar-container  .player-controls button[class="control-button spoticon-pause-16 control-button--circled"]\')'
     }, function (res) {
-      var status = res[0] ? 'playing' : 'paused'
+      var status = res[0] ? 'playing' : 'paused';
 
       callback(status)
     })
   },
 
   click: function (options, callback) {
-    var tab = options.tab
-    var query = options.query
-    var code = 'document.querySelector(\'' + query + '\').click()'
+    var tab = options.tab;
+    var query = options.query;
+    var code = 'document.querySelector(\'' + query + '\').click()';
 
     Chrome.executeScript({
       tab: tab,
@@ -92,70 +92,70 @@ var Spotify = {
   pause: function (tab, callback) {
     Spotify.click({
       tab: tab,
-      query: '.nowPlayingBar-container .player-controls button[class^="control-button spoticon-pause"]'
+      query: '.now-playing-bar-container  .player-controls button[class="control-button spoticon-pause-16 control-button--circled"]'
     }, callback)
   },
 
   play: function (tab, callback) {
     Spotify.click({
       tab: tab,
-      query: '.nowPlayingBar-container .player-controls button[class^="control-button spoticon-play"]'
+      query: '.now-playing-bar-container  .player-controls button[class="control-button spoticon-play-16 control-button--circled"]'
     }, callback)
   },
 
   previous: function (tab, callback) {
     Spotify.click({
       tab: tab,
-      query: '.nowPlayingBar-container .player-controls button[class^="control-button spoticon-skip-back"]'
+      query: '.now-playing-bar-container .player-controls button[class="control-button spoticon-skip-back-16"]'
     }, callback)
   },
 
   next: function (tab, callback) {
     Spotify.click({
       tab: tab,
-      query: '.nowPlayingBar-container .player-controls button[class^="control-button spoticon-skip-forward"]'
+      query: '.now-playing-bar-container .player-controls button[class="control-button spoticon-skip-forward-16"]'
     }, callback)
   }
-}
+};
 
 var App = {
   setTheme: function (theme) {
     var bodyColor = theme === 'light'
       ? 'Dark'
-      : 'Light'
+      : 'Light';
 
-    document.body.setAttribute('class', theme)
+    document.body.setAttribute('class', theme);
     findEl('#color-body').innerHTML = bodyColor
   }
-}
+};
 
-function changeColor () {
+function changeColor() {
   if (document.body.className === 'light') {
-    document.body.setAttribute('class', 'dark')
-    document.getElementById('color-body').innerHTML = 'Light'
+    document.body.setAttribute('class', 'dark');
+    document.getElementById('color-body').innerHTML = 'Light';
     chrome.storage.sync.set({'color': 'dark'})
   } else {
-    document.body.setAttribute('class', 'light')
-    document.getElementById('color-body').innerHTML = 'Dark'
+    document.body.setAttribute('class', 'light');
+    document.getElementById('color-body').innerHTML = 'Dark';
     chrome.storage.sync.set({'color': 'light'})
   }
 }
 
-function fetchLyrics () {
+function fetchLyrics() {
   // get artist and trackname
-  var artist = findEl('#current-track-artist').innerHTML
-  var track = findEl('#current-track-name').innerHTML
-  var xhr = new XMLHttpRequest()
+  var artist = findEl('#current-track-artist').innerText;
+  var track = findEl('#current-track-name').innerText;
+  var xhr = new XMLHttpRequest();
 
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
-      var response = JSON.parse(xhr.responseText)
+      var response = JSON.parse(xhr.responseText);
 
-      findEl('#container-lyrics').style.display = 'flex'
-      findEl('#container-lyrics').innerHTML = (response.mus && response.mus[0] ? response.mus[0].text : 'Sorry, no lyric available for this song.')
+      findEl('#container-lyrics').style.display = 'flex';
+      findEl('#container-lyrics').innerText = (response.mus && response.mus[0] ? response.mus[0].text : 'Sorry, no lyric available for this song.')
     }
-  }
-  xhr.open('GET', 'https://api.vagalume.com.br/search.php?art=' + artist + '&mus=' + track + '&apikey=' + VAGALUME_API)
+  };
+  xhr.open('GET', 'https://api.vagalume.com.br/search.php?art=' + artist + '&mus=' + track + '&apikey=' + VAGALUME_API);
   xhr.send()
 }
 
@@ -163,24 +163,24 @@ function fetchLyrics () {
  * Fetch spotify element class (from id)
  * return: object with classnames as attributes and value=true
  */
-function fetchClassNamesById (tab, id, callback) {
+function fetchClassNamesById(tab, id, callback) {
   chrome.tabs.executeScript(tab, {
     code: APP_PLAYER + '.getElementById("' + id + '").className'
   }, function (response) {
-    var classNamesList = String(response).split(' ')
-    var classNamesObject = {} // object with className:true
+    var classNamesList = String(response).split(' ');
+    var classNamesObject = {};  // object with className:true
 
     classNamesList.forEach(function (className) {
       classNamesObject[className] = true
-    })
+    });
 
     callback(classNamesObject)
   })
 }
 
-function fetchControlState (tab, control, callback) {
+function fetchControlState(tab, control, callback) {
   fetchClassNamesById(tab, control, function (response) {
-    var state = 'active'
+    var state = 'active';
 
     if (response.disabled) {
       state = 'disabled'
@@ -190,37 +190,37 @@ function fetchControlState (tab, control, callback) {
   })
 }
 
-function togglePlayPause () {
+function togglePlayPause() {
   for (var tab of State.tabs) {
     Spotify.getPlayOrPauseStatus(tab, function (status) {
       var toggle = {
         paused: 'playing',
         playing: 'paused',
         disabled: 'disabled'
-      }
+      };
 
       renderPlayPauseState(toggle[status])
     })
   }
 }
 
-function renderAlbumArt (albumArtURL) {
-  findEl('#background-album').style.background = 'url(' + albumArtURL + ')'
+function renderAlbumArt(albumArtURL) {
+  findEl('#background-album').style.background = 'url(' + albumArtURL + ')';
   findEl('#album-art').innerHTML = '<img src=' + albumArtURL + ' style="width:180px; height:180px">'
 }
 
-function renderTrackName (name) {
+function renderTrackName(name) {
   console.log('renderTrackName', name);
   findEl('#current-track-name').innerHTML = name || '-'
 }
 
-function renderTrackArtist (artist) {
+function renderTrackArtist(artist) {
   console.log('renderTrackArtist', artist);
   findEl('#current-track-artist').innerHTML = artist || '-';
 }
 
-function renderControlState (control, state) {
-  var controlClassName = 'controller__skip-back'
+function renderControlState(control, state) {
+  var controlClassName = 'controller__skip-back';
 
   if (state === 'disabled') {
     controlClassName += ' disabled'
@@ -229,18 +229,18 @@ function renderControlState (control, state) {
   document.getElementById(control).className = controlClassName
 }
 
-function renderPlayPauseState (state) {
-  var playPauseDOM = findEl('#play-pause')
-  var playPauseClassName = 'glyphicon glyphicon-play'
+function renderPlayPauseState(state) {
+  var playPauseDOM = findEl('#play-pause');
+  var playPauseClassName = 'glyphicon glyphicon-play';
 
   switch (state) {
     case 'playing':
-      playPauseClassName = 'glyphicon glyphicon-pause'
-      playPauseDOM.removeAttribute('disabled', true)
-      break
+      playPauseClassName = 'glyphicon glyphicon-pause';
+      playPauseDOM.removeAttribute('disabled', true);
+      break;
     case 'paused':
-      playPauseDOM.removeAttribute('disabled', true)
-      break
+      playPauseDOM.removeAttribute('disabled', true);
+      break;
     case 'disabled':
       playPauseDOM.setAttribute('disabled', true)
   }
@@ -248,10 +248,10 @@ function renderPlayPauseState (state) {
   playPauseDOM.getElementsByTagName('span')[0].className = playPauseClassName
 }
 
-function execute (action) {
+function execute(action) {
   State.tabs.forEach(function (tab) {
     Spotify[action](tab)
-  })
+  });
 
   // Update current trackname
   setTimeout(updateTrackInfo, 2000)
@@ -260,71 +260,71 @@ function execute (action) {
 /**
  * Show current trackname if there's one
  */
-function updateTrackInfo () {
+function updateTrackInfo() {
   for (var tab of State.tabs) {
-    Spotify.getAlbumArt(tab, renderAlbumArt)
-    Spotify.getTrackName(tab, renderTrackName)
-    Spotify.getArtistName(tab, renderTrackArtist)
+    Spotify.getAlbumArt(tab, renderAlbumArt);
+    Spotify.getTrackName(tab, renderTrackName);
+    Spotify.getArtistName(tab, renderTrackArtist);
 
-    fetchControlState(tab.id, 'previous', renderControlState)
-    fetchControlState(tab.id, 'next', renderControlState)
+    fetchControlState(tab.id, 'previous', renderControlState);
+    fetchControlState(tab.id, 'next', renderControlState);
 
     Spotify.getPlayOrPauseStatus(tab, renderPlayPauseState)
   }
 }
 
-function fetchTheme (callback) {
+function fetchTheme(callback) {
   chrome.storage.sync.get('color', function (item) {
     callback(item.color || 'dark')
   })
 }
 
-function setInitialTheme () {
+function setInitialTheme() {
   fetchTheme(App.setTheme)
 }
 
 // this method is called on background.js to toggle between play/pause;
-function handlePlayOrPauseClick () {
+function handlePlayOrPauseClick() {
   State.tabs.forEach(function (tab) {
     Spotify.getPlayOrPauseStatus(tab, function (status) {
       return status === 'playing'
         ? execute('pause')
         : execute('play')
-    })
+    });
     togglePlayPause()
   })
 }
 
-function handleLogoClick () {
+function handleLogoClick() {
   return State.tabs.length
     ? Spotify.openTab(State.tabs)
     : Spotify.createTab()
 }
 
-function setInitialState (callback) {
+function setInitialState(callback) {
   Spotify.getCurrentTab(function (tabs) {
-    State.tabs = tabs
+    State.tabs = tabs;
 
     callback()
   })
-};
+}
 
 // init
 document.addEventListener('DOMContentLoaded', function () {
   setInitialState(function () {
-    updateTrackInfo()
-    setInitialTheme()
+    updateTrackInfo();
+    setInitialTheme();
 
     // events
-    onClick(findEl('#open'), handleLogoClick)
-    onClick(findEl('#color-body'), changeColor)
-    onClick(findEl('#play-pause'), handlePlayOrPauseClick)
+    onClick(findEl('#open'), handleLogoClick);
+    onClick(findEl('#color-body'), changeColor);
+    onClick(findEl('#play-pause'), handlePlayOrPauseClick);
     onClick(findEl('#previous'), function () {
       execute('previous')
-    })
+    });
     onClick(findEl('#next'), function () {
       execute('next')
-    })
+    });
     onClick(findEl('#show-lyrics'), fetchLyrics)
   })
-})
+});
